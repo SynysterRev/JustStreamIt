911478cb5faff89c0573909623632c6904de1be6
Michael <56340359+SynysterRev@users.noreply.github.com>
2024-12-20 15:18:59 +0100

    remove ph text and wait movie to be loaded to display modal

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,10 +1,11 @@
 async function displayPopup(movieId){
+    let movie = await getMovie(movieId);
+
     let popup = document.querySelector("#popup-background");
     popup.classList.toggle("hidden");
     let body = document.querySelector("body");
     body.classList.toggle("overflow-hidden");
 
-    let movie = await getMovie(movieId);
     popup.querySelector("#movie-title").innerText = movie["original_title"];
     popup.querySelector("#movie-description").innerText = movie["long_description"];
     let images = popup.querySelectorAll("img");
@@ -29,6 +30,10 @@ async function displayPopup(movieId){
 
     let movieScore = popup.querySelector("#movie-score");
     movieScore.innerText = "IMDB score: " + movie["imdb_score"] + "/10";
+
+    let movieRevenue = popup.querySelector("#movie-revenue");
+    let revenue = movie["worldwide_gross_income"];
+    movieRevenue.innerText = (revenue !== null) ? "Revenue : " + movie["worldwide_gross_income"] : "";
 
     let movieReal = popup.querySelector("#movie-real");
     movieReal.innerText = movie["directors"].toString().replaceAll(",", ", ");
